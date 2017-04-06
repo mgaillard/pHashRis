e734cd44aa4c2e02fa569ddb8d2f07380214a82d
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2017-04-06 17:25:36 +0200

    Add rotations and the possibility to choose which part of the benchmark to execute

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -16,51 +16,57 @@ for function in ${functions[@]}; do
     trace "Index: Base"
     ./"$ris_app" -f "$function" index "$benchmark_base_dir"
 
-
-    # Search and compute the measures for individual transformations
-    trace "Search individual transformations:"
-    # Non-indexed
-    (
-        ./"$ris_app" -f "$function" search "$benchmark_non_indexed_dir" -t ${end_threshold[$function]}
-    ) | ../RisBenchmarkResult --relevant_items 1 --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_${function}_non-indexed.dat"
-    gnuplot -e "filename='result_${function}_non-indexed.dat';name='${function} (non-indexed)'" "../threshold.pg" > "threshold_${function}_non-indexed.png"
-    # Bases
-    (
-        ./"$ris_app" -f "$function" search "$benchmark_base_dir" -t ${end_threshold[$function]}
-    ) | ../RisBenchmarkResult --relevant_items 1 --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_${function}_base.dat"
-    gnuplot -e "filename='result_${function}_base.dat';name='${function} (base)'" "../threshold.pg" > "threshold_${function}_base.png"
-    # Transformations
-    for t in ${transformations[@]}; do
+    # Benchmark on individual transformations
+    if [ "$individual_transformation_benchmark" = true ] ; then
+        # Search and compute the measures for individual transformations
+        trace "Benchmark individual transformations:"
+        # Non-indexed
         (
-            ./"$ris_app" -f "$function" search "$t" -t ${end_threshold[$function]}
-        ) | ../RisBenchmarkResult --relevant_items 1 --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_${function}_$t.dat"
-        gnuplot -e "filename='result_${function}_$t.dat';name='${function} ($t)'" "../threshold.pg" > "threshold_${function}_$t.png"
-    done
-
-
-    # Index the transformed images
-    for t in ${transformations[@]}; do
-        trace "Index: ${transformation_name[$t]}"
-        ./"$ris_app" -f "$function" index "$t"
-    done
-
-
-    # Search and compute the measures for all transformations
-    (
-        ./"$ris_app" -f "$function" search "$benchmark_base_dir" -t ${end_threshold[$function]};
+            ./"$ris_app" -f "$function" search "$benchmark_non_indexed_dir" -t ${end_threshold[$function]}
+        ) | ../RisBenchmarkResult --relevant_items 1 --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_${function}_non-indexed.dat"
+        gnuplot -e "filename='result_${function}_non-indexed.dat';name='${function} (non-indexed)'" "../threshold.pg" > "threshold_${function}_non-indexed.png"
+        # Bases
+        (
+            ./"$ris_app" -f "$function" search "$benchmark_base_dir" -t ${end_threshold[$function]}
+        ) | ../RisBenchmarkResult --relevant_items 1 --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_${function}_base.dat"
+        gnuplot -e "filename='result_${function}_base.dat';name='${function} (base)'" "../threshold.pg" > "threshold_${function}_base.png"
+        # Transformations
         for t in ${transformations[@]}; do
-            ./"$ris_app" -f "$function" search "$t" -t ${end_threshold[$function]};
-        done;
-        ./"$ris_app" -f "$function" search "$benchmark_non_indexed_dir" -t ${end_threshold[$function]}
-    ) | ../RisBenchmarkResult --relevant_items $relevant_items --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_$function.dat"
-    gnuplot -e "filename='result_$function.dat';name='${function}'" "../threshold.pg" > "threshold_$function.png"
+            (
+                ./"$ris_app" -f "$function" search "$t" -t ${end_threshold[$function]}
+            ) | ../RisBenchmarkResult --relevant_items 1 --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_${function}_$t.dat"
+            gnuplot -e "filename='result_${function}_$t.dat';name='${function} ($t)'" "../threshold.pg" > "threshold_${function}_$t.png"
+        done
+    fi
+
+    # Benchmark on all modifications at the same time
+    if [ "$multiple_transformation_benchmark" = true ] ; then
+        # Index the transformed images
+        for t in ${transformations[@]}; do
+            trace "Index: ${transformation_name[$t]}"
+            ./"$ris_app" -f "$function" index "$t"
+        done
+
+
+        # Search and compute the measures for all transformations
+        (
+            ./"$ris_app" -f "$function" search "$benchmark_base_dir" -t ${end_threshold[$function]};
+            for t in ${transformations[@]}; do
+                ./"$ris_app" -f "$function" search "$t" -t ${end_threshold[$function]};
+            done;
+            ./"$ris_app" -f "$function" search "$benchmark_non_indexed_dir" -t ${end_threshold[$function]}
+        ) | ../RisBenchmarkResult --relevant_items $relevant_items --start_threshold ${start_threshold[$function]} --step_threshold ${step_threshold[$function]} --end_threshold ${end_threshold[$function]} > "result_$function.dat"
+        gnuplot -e "filename='result_$function.dat';name='${function}'" "../threshold.pg" > "threshold_$function.png"
+    fi
 
     # Remove index file
     rm index
 done
 
-# Plot results
-trace "Plot results"
-gnuplot "../roc.pg" > "roc.png"
+if [ "$multiple_transformation_benchmark" = true ] ; then
+    # Plot results
+    trace "Plot results"
+    gnuplot "../roc.pg" > "roc.png"
+fi
 
 trace "END"
--- benchmark/config.sh
+++ benchmark/config.sh
@@ -22,16 +22,29 @@ benchmark_dir="benchmark"
 benchmark_base_dir="base"
 benchmark_non_indexed_dir="non-indexed"
 
+# Functions to benchmark
 # Possible values: "DCT" "MH" "RV"
 declare -a functions=("DCT" "MH" "RV")
-# Possible values: "blur" "gray" "half" "compress10" "rotate5" "crop10"
+
+# Modifications to apply on images
+# Possible values: "blur" "gray" "half" "compress10" "rotate1" "rotate2" "rotate3" "rotate4" "rotate5" "crop10"
 declare -a transformations=("blur" "gray" "half" "compress10" "rotate5" "crop10")
+
+# Set to "true" to perform a benchmark on individual transformations
+individual_transformation_benchmark=true
+
+# Set to "true" to performa a benchmark on all modifications at the same time
+multiple_transformation_benchmark=true
 
 declare -A transformation_name
 transformation_name["blur"]="Blur"
 transformation_name["gray"]="Grayscale"
 transformation_name["half"]="Half size"
 transformation_name["compress10"]="Compress with quality 10"
+transformation_name["rotate1"]="Rotation with angle 1"
+transformation_name["rotate2"]="Rotation with angle 2"
+transformation_name["rotate3"]="Rotation with angle 3"
+transformation_name["rotate4"]="Rotation with angle 4"
 transformation_name["rotate5"]="Rotation with angle 5"
 transformation_name["crop10"]="Crop right 10%"
 
@@ -40,6 +53,10 @@ transformation_command["blur"]="mogrify -path blur -blur 4x2 $benchmark_base_dir
 transformation_command["gray"]="mogrify -path gray -type Grayscale $benchmark_base_dir/*"
 transformation_command["half"]="mogrify -path half -resize 50% $benchmark_base_dir/*"
 transformation_command["compress10"]="mogrify -path compress10 -format jpg -quality 10 $benchmark_base_dir/*"
+transformation_command["rotate1"]="mogrify -path rotate1 -rotate 1 $benchmark_base_dir/*"
+transformation_command["rotate2"]="mogrify -path rotate2 -rotate 2 $benchmark_base_dir/*"
+transformation_command["rotate3"]="mogrify -path rotate3 -rotate 3 $benchmark_base_dir/*"
+transformation_command["rotate4"]="mogrify -path rotate4 -rotate 4 $benchmark_base_dir/*"
 transformation_command["rotate5"]="mogrify -path rotate5 -rotate 5 $benchmark_base_dir/*"
 transformation_command["crop10"]="mogrify -path crop10 -crop 90%x100%+0+0 $benchmark_base_dir/*"
 
